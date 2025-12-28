c21fcc8bbdf9bf45a7ead0cf63a2f7ceca90721a
metal <metalheadkrn@gmail.com>
2025-12-28 18:58:09 +0530

    Creating repo for aws ssm parameter for fetching passwords using terraform

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "key_id" {
+  default = "arn:aws:kms:us-east-1:367241114876:key/b0eaa327-c037-47e6-93ed-78b8b08219b9"
+}
+
+variable "parameters" {
+  default = {
+    "dev.expense.rds.username" = "admin1"
+  }
+}
+
+## Note: In organizations we dont keep passwords in git repository at any cpst
+variable "secrets" {
+  default = {
+    "dev.expense.rds.password" = "ExpenseApp123"
+  }
+}
