8e1f8798814f47505256b24a526104552f3187ec
metal <metalheadkrn@gmail.com>
2026-01-04 17:39:25 +0530

    Fetching password for new RDS endpoint and changing the login in backend main.yml task

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "key_id" {
 variable "parameters" {
   default = {
     "dev.expense.rds.username" = "admin1"
+    "dev.expense.rds.endpoint" = "dev-expense-rds.ca1a4y2cuoey.us-east-1.rds.amazonaws.com"
   }
 }
 
