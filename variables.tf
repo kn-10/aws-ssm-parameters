9396e5f1ebd2a7e7c3cfa91f816cb447477668fe
metal <metalheadkrn@gmail.com>
2026-01-08 14:17:56 +0530

    Module backend depends on module rds as mysql should be running before and so we are defining dependency here

--- variables.tf
+++ variables.tf
@@ -6,6 +6,8 @@ variable "parameters" {
   default = {
     "dev.expense.rds.username" = "admin1"
     "dev.expense.rds.endpoint" = "dev-expense-rds.ca1a4y2cuoey.us-east-1.rds.amazonaws.com"
+    "dev.expense.rds.username"              = "admin1"
+    "dev.expense.rds.endpoint" = "dev-expense-rds.ca1a4y2cuoey.us-east-1.rds.amazonaws.com"
     "dev.expense.frontend.backend_endpoint" = "https://backend-dev.kdevops.online/"
   }
 }
