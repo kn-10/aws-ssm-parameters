16183a48c27e5e7747694dba85720478a76707eb
metal <metalheadkrn@gmail.com>
2026-01-28 19:07:02 +0530

    Adding SES Secrets

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "secrets" {
     "dev.expense.rds.password" = "ExpenseApp123"
     "ssh.password"             = "DevOps321"
     "ssh.username"             = "centos"
-    "ses.username"             = "ses-smtp-user.20260128-145034"
-    "ses.password"             = "BH0n8A+QQbvXN3bT8kMC9QODs5Lkutv3vlVgavXwfNLG"
+    "ses.username"             = "AKIAVLAKGWT6AV277TWX"
+    "ses.password"             = "BCdJN2ZbQErs41ZSlRuiMp0wk4FCkT5M7tD31+VzDjbl"
   }
 }
