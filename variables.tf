34ff45c4133f2bbadd2cac39efed8402c8b4841d
metal <metalheadkrn@gmail.com>
2026-03-09 19:51:32 +0530

    Updated password for elasticsearch

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "secrets" {
     "grafana.apikey"                    = "glsa_uofvreAAKcDSZcrD2IYh7b967M72obgp_cd298dc1"
     "dev.expense.frontend.newrelic.key" = "NRAK-ZD1XNOBKSEGBDNWD2KHGUL2JVIR"
     "dev.expense.backend.newrelic.key"  = "413d2388ea45416359eb026782087a44FFFFNRAL"
-    "elasticsearch.password"            = "rLRyYZ=hiTQhM37k1whp"
+    "elasticsearch.password"            = "2-djhAKjES34JYLPKTAq"
     "artifactory.user"                  = "admin"
     "artifactory.password"              = "Admin123"
   }
