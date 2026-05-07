bfdfc99732be6f9a052c065bd4a8ec47f594843d
metal <metalheadkrn@gmail.com>
2026-05-07 17:21:01 +0530

    Masking the passwords in logs or runtime

--- variables.tf
+++ variables.tf
@@ -3,6 +3,7 @@ variable "key_id" {
 }
 
 variable "parameters" {
+  sensitive = true # This prevents the values from being printed in the CLI
   default = {
     "dev.expense.rds.username"               = "admin1"
     "dev.expense.rds.endpoint"               = "dev-expense-rds.ca1a4y2cuoey.us-east-1.rds.amazonaws.com"
