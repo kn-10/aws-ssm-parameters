84b07b59d440559cf57133fb96bb9d76cb5bdd76
metal <metalheadkrn@gmail.com>
2026-05-09 18:20:12 +0530

    Masking secrets in parameter store

--- variables.tf
+++ variables.tf
@@ -2,8 +2,7 @@ variable "key_id" {
   default = "arn:aws:kms:us-east-1:367241114876:key/b0eaa327-c037-47e6-93ed-78b8b08219b9"
 }
 
-variable "parameters" {
-  sensitive = true # This prevents the values from being printed in the CLI
+variable "parameters" { # This prevents the values from being printed in the CLI
   default = {
     "dev.expense.rds.username"               = "admin1"
     "dev.expense.rds.endpoint"               = "dev-expense-rds.ca1a4y2cuoey.us-east-1.rds.amazonaws.com"
