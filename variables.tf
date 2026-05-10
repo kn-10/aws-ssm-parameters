3573f5401c8e43e378a2bf489bf4320ea7b423fe
metal <metalheadkrn@gmail.com>
2026-05-10 12:46:42 +0530

    Masking secrets in parameter store

--- variables.tf
+++ variables.tf
@@ -2,7 +2,8 @@ variable "key_id" {
   default = "arn:aws:kms:us-east-1:367241114876:key/b0eaa327-c037-47e6-93ed-78b8b08219b9"
 }
 
-variable "parameters" { # This prevents the values from being printed in the CLI
+variable "parameters" {
+
   default = {
     "dev.expense.rds.username"               = "admin1"
     "dev.expense.rds.endpoint"               = "dev-expense-rds.ca1a4y2cuoey.us-east-1.rds.amazonaws.com"
@@ -42,6 +43,7 @@ variable "parameters" { # This prevents the values from being printed in the CLI
 
 ## Note: In organizations we don't keep passwords in git repository at any cpst
 variable "secrets" {
+  sensitive = true # This prevents the values from being printed in the CLI
   default = {
     "dev.expense.rds.password"          = "ExpenseApp123"
     "ssh.password"                      = "DevOps321"
